67a43271256287f00f88c9fb6b01b323d47489ea
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-09-10 01:35:35 +0800

    video done

--- components/video/VideoCard.tsx
+++ components/video/VideoCard.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { video } from '../../Type/videoProps';
 import { HiVolumeOff, HiVolumeUp } from 'react-icons/hi';
 import { BsFillPlayFill, BsPlay, BsFillPauseFill } from 'react-icons/bs';
@@ -12,7 +12,20 @@ interface IProps {
   key: string;
 }
 const VideoCard: NextPage<IProps> = ({ post, key }) => {
-  console.log(post);
+  const [isHover, setIsHover] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const vidRef = useRef<HTMLVideoElement>(null);
+  const onVideoClick = () => {
+    if (isPlaying) {
+      vidRef.current?.pause();
+      setIsPlaying(false);
+    } else {
+      vidRef.current?.play();
+      setIsPlaying(true);
+    }
+  };
+
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -24,6 +37,60 @@ const VideoCard: NextPage<IProps> = ({ post, key }) => {
               </>
             </Link>
           </div>
+          <div>
+            <Link href="/">
+              <div className="flex items-center gap-2">
+                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
+                  {post.postedBy.userName} {'  '}
+                  <GoVerified className="text-[#EA5666] text-md" />
+                </p>
+                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">{post.postedBy.userName}</p>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="lg:ml-20 flex gap-4  relative ">
+        <div
+          onMouseEnter={() => {
+            setIsHover(true);
+          }}
+          onMouseLeave={() => {
+            setIsHover(false);
+          }}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              loop
+              ref={vidRef}
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-200 rounded-2xl  cursor-pointer bg-gray-200"
+              src={post.video.asset.url}
+            ></video>
+          </Link>
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
+              {isPlaying ? (
+                <button onClick={onVideoClick}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={onVideoClick}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+              {isMuted ? (
+                <button onClick={() => setIsMuted(false)}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />{' '}
+                </button>
+              ) : (
+                <button onClick={() => setIsMuted(true)}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
