d178b0f00abf1133a46fb13575ded80fb0204f08
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-09-19 00:00:02 +0800

    search

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,10 +3,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../../utils/mitch.png';
 import UserLogin from './UserLogin';
+import { CgSearchLoading } from 'react-icons/cg';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
+  const router = useRouter();
   const [search, setSearch] = useState<string>('');
-  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {};
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    if (search === '') {
+      return;
+    } else if (search !== '') {
+      router.push(`/search/${search}`);
+    }
+  };
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-1 px-4">
       <Link href="/">
@@ -14,15 +24,23 @@ const Navbar = () => {
           <Image src={logo} alt="logo" className="cursor-pointer" layout="responsive" />
         </div>
       </Link>
+
       <div className="relative hidden md:block">
         <form onSubmit={handleSearch} className="absolute md:static top-10 -left-20 my-2 bg-white">
           <input
             type="text"
             value={search}
-            onChange={() => {}}
+            onChange={(e) => setSearch(e.target.value)}
             placeholder="Search"
             className="bg-primary p-2 md:text-md font-medium border-2 border-gray-10 focus:outline-none focus:border-2 focus:border-gray-300 rounded-full md:top-0 w-[300px] md:w-[400px]"
           />
+          <button
+            type="submit"
+            className="absolute md:right-5 right-4 top-4 border-l-2 border-gray-300 pl-4 text-3xl text-gray-400"
+            onClick={() => handleSearch}
+          >
+            <CgSearchLoading />
+          </button>
         </form>
       </div>
       <UserLogin />
---
+++ pages/search/[searchId].tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Link from 'next/link';
+import axios from 'axios';
+
+import useAuthStore from '../../store/authStore';
+import { video } from '../../Type/videoProps';
+import { userDetail } from '../../Type/userDetail';
+import { BASE_URL } from '../api/post';
+
+import NoResults from '../../components/video/NoResults';
+import VideoCard from '../../components/video/VideoCard';
+
+const Search = ({ videos }: { videos: video[] }) => {
+  const [isAccounts, setIsAccounts] = useState(false);
+  const { allUsers }: { allUsers: userDetail[] } = useAuthStore();
+  const router = useRouter();
+  const { searchId }: any = router.query;
+
+  const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+  const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+  const searchedAccounts = allUsers?.filter((user: userDetail) => user.userName.toLowerCase().includes(searchId.toLowerCase()));
+  console.log(videos);
+
+  return (
+    <div className="w-full">
+      <div className="flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full">
+        <p className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`} onClick={() => setIsAccounts(true)}>
+          Accounts
+        </p>
+        <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+          Videos
+        </p>
+      </div>
+      {isAccounts ? (
+        <div className="md:mt-16">
+          {searchedAccounts.length > 0 ? (
+            searchedAccounts.map((user: userDetail, idx: number) => (
+              <Link key={idx} href={`/profile/${user._id}`}>
+                <div className=" flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+                  <div>
+                    <Image width={50} height={50} className="rounded-full" alt="user-profile" src={user.image} />
+                  </div>
+                  <div>
+                    <div>
+                      <p className="flex gap-1 items-center text-lg font-bold text-primary">
+                        {user.userName} <GoVerified className="text-blue-400" />
+                      </p>
+                      <p className="capitalize text-gray-400 text-sm">{user.userName}</p>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <NoResults text={`No Account Results for ${searchId}`} />
+          )}
+        </div>
+      ) : (
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start ">
+          {videos.length ? (
+            videos.map((post: video, idx: number) => <VideoCard post={post} key={idx} />)
+          ) : (
+            <NoResults text={`No Video Results for ${searchId}`} />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({ params: { searchId } }: { params: { searchId: string } }) => {
+  const res = await axios.get(`${BASE_URL}/api/search/${searchId}`);
+  console.log(res.data);
+  return {
+    props: { videos: res.data },
+  };
+};
+
+export default Search;
