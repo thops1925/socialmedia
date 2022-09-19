9da7c925c31211f2cca7229f3720f063c0ec7679
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-09-20 04:03:06 +0800

    deploy

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-1 px-4">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-4 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[130px]">
           <>
